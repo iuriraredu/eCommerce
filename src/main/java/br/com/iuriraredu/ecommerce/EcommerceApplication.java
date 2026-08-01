bfbdd69484b6a4f2ee6b22832f4d5092469e11bf
iuriraredu <iuri.ramos.reducino@gmail.com>
2026-08-01 15:45:31 -0300

    feat: implementa CRUD de clientes e refatora arquitetura de produtos

--- src/main/java/br/com/iuriraredu/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/iuriraredu/ecommerce/EcommerceApplication.java
@@ -1,5 +1,12 @@
 package br.com.iuriraredu.ecommerce;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EcommerceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 }
